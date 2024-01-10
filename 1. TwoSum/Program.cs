e3eac73747568d5574b76b0d58211b31ffa385a6
SimonAlphonse <91269886+SimonAlphonse@users.noreply.github.com>
2024-01-10 11:31:00 +0530

    Initial Commit

---
+++ 1. TwoSum/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello, World!");
+        }
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+
+        }
+    }
+}
