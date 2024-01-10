115b980f793b858367a3ccd7e3dc430abc89fce9
SimonAlphonse <91269886+SimonAlphonse@users.noreply.github.com>
2024-01-10 17:59:07 +0530

    Refactoring

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,4 +1,4 @@
-using TwoSum;
+using MyLeetCode;
 
 namespace Tests
 {
@@ -9,7 +9,7 @@ namespace Tests
         [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
         [InlineData(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
         [InlineData(new int[] { 3, 2, 3 }, 6, new int[] { 0, 2 })]
-        public void IsValid(int[] nums, int target, int[] output)
+        public void TwoSum(int[] nums, int target, int[] output)
         {
             var result = Solution.TwoSum(nums, target);
             Assert.Equal(result, output);
