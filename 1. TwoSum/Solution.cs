8218d8dcfc2b607230e75b558e4e3b8e484d9f5a
SimonAlphonse <91269886+SimonAlphonse@users.noreply.github.com>
2024-01-10 14:03:56 +0530

    First Answer

---
+++ 1. TwoSum/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace TwoSum
+{
+    public static class Solution
+    {
+        public static void Main()
+        {
+
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int index = 0; index < nums.Length; index++)
+            {
+                var matchIndex = nums[(index + 1)..].ToList().IndexOf(target - nums[index]);
+                if (matchIndex != -1)
+                    return [index, matchIndex + index + 1];
+            }
+
+            return [];
+        }
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,18 @@
+using TwoSum;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Theory]
+        [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+        [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
+        [InlineData(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+        [InlineData(new int[] { 3, 2, 3 }, 6, new int[] { 0, 2 })]
+        public void IsValid(int[] nums, int target, int[] output)
+        {
+            var result = Solution.TwoSum(nums, target);
+            Assert.Equal(result, output);
+        }
+    }
+}
